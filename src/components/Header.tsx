a2caae88493c80897284c0e1f83514154f657321
Davi Alpiano <davialpiano@gmail.com>
2024-03-10 17:00:05 -0300

    Req 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,20 @@
+import { useSelector } from "react-redux";
+import { User } from "../types";
+
+type infoUser = {
+  user: User
+}
+
 function Header() {
-  return <div>Header</div>;
+  const infoUser = useSelector((state: infoUser) => state.user);
+
+  return (
+    <header>
+    <h3 data-testid="email-field">{ infoUser.email }</h3>
+    <h3 data-testid="total-field">0</h3>
+    <h3 data-testid="header-currency-field">BRL</h3>
+    </header>
+  );
 }
 
 export default Header;
