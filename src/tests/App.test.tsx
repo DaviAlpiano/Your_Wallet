bcf80243ef29c72930f47818bb81fea5ca9aecda
Davi Alpiano <davialpiano@gmail.com>
2024-03-13 14:49:01 -0300

    att lint test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,64 +1,64 @@
-import { fireEvent, screen } from "@testing-library/dom";
+import { fireEvent, screen } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
-import App from "../App"
-import { renderWithRouterAndRedux } from "./helpers/renderWith"
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
 
 describe('01 - Testando a tela de login.', () => {
-    it('Encontrar os elementos login e senha.', () => {
-        renderWithRouterAndRedux(<App/>);
+  it('Encontrar os elementos login e senha.', () => {
+    renderWithRouterAndRedux(<App />);
 
-        const email = screen.getByRole('textbox', {  name: /email:/i});
-        const senha = screen.getByLabelText(/senha:/i);
+    const email = screen.getByRole('textbox', { name: /email:/i });
+    const senha = screen.getByLabelText(/senha:/i);
 
-        expect(email).toBeInTheDocument();
-        expect(senha).toBeInTheDocument();
-    });
+    expect(email).toBeInTheDocument();
+    expect(senha).toBeInTheDocument();
+  });
 
-    it('Existe o botão de login na página.', () => {
-        renderWithRouterAndRedux(<App/>);
+  it('Existe o botão de login na página.', () => {
+    renderWithRouterAndRedux(<App />);
 
-        const buttaoLogin = screen.getByRole('button', {  name: /entrar/i});
+    const buttaoLogin = screen.getByRole('button', { name: /entrar/i });
 
-        expect(buttaoLogin).toBeInTheDocument();
-    });
+    expect(buttaoLogin).toBeInTheDocument();
+  });
 
-    it('O botão esta desabilitado sem as informações corretas de login.', () => {
-        renderWithRouterAndRedux(<App/>);
+  it('O botão esta desabilitado sem as informações corretas de login.', () => {
+    renderWithRouterAndRedux(<App />);
 
-        const buttaoLogin = screen.getByRole('button', {  name: /entrar/i});
-        const email = screen.getByRole('textbox', {  name: /email:/i});
-        const senha = screen.getByLabelText(/senha:/i);
+    const buttaoLogin = screen.getByRole('button', { name: /entrar/i });
+    const email = screen.getByRole('textbox', { name: /email:/i });
+    const senha = screen.getByLabelText(/senha:/i);
 
-        expect(buttaoLogin).toBeDisabled();
+    expect(buttaoLogin).toBeDisabled();
 
-        fireEvent.change(email, {target:{ value : 'ronaldo@gmail.com' } });
-        fireEvent.change(senha, {target:{ value : '123456' } });
+    fireEvent.change(email, { target: { value: 'ronaldo@gmail.com' } });
+    fireEvent.change(senha, { target: { value: '123456' } });
 
-        expect(buttaoLogin).not.toBeDisabled();
+    expect(buttaoLogin).not.toBeDisabled();
 
-        fireEvent.change(senha, {target:{ value : '12345' } });
-        expect(buttaoLogin).toBeDisabled();
+    fireEvent.change(senha, { target: { value: '12345' } });
+    expect(buttaoLogin).toBeDisabled();
 
-        fireEvent.change(email, {target:{ value : 'ronaldogmail.com' } });
-        fireEvent.change(senha, {target:{ value : '123456' } });
+    fireEvent.change(email, { target: { value: 'ronaldogmail.com' } });
+    fireEvent.change(senha, { target: { value: '123456' } });
 
-        expect(buttaoLogin).toBeDisabled();
-    });
+    expect(buttaoLogin).toBeDisabled();
+  });
 
-    it('Ao inserir os dados corretor e clicar no botao de entrar, é redirecionado pra pagina wallet', async () => {
-        renderWithRouterAndRedux(<App/>);
+  it('Ao inserir os dados corretor e clicar no botao de entrar, é redirecionado pra pagina wallet', async () => {
+    renderWithRouterAndRedux(<App />);
 
-        const buttaoLogin = screen.getByRole('button', {  name: /entrar/i});
-        const email = screen.getByRole('textbox', {  name: /email:/i});
-        const senha = screen.getByLabelText(/senha:/i);
+    const buttaoLogin = screen.getByRole('button', { name: /entrar/i });
+    const email = screen.getByRole('textbox', { name: /email:/i });
+    const senha = screen.getByLabelText(/senha:/i);
 
-        fireEvent.change(email, {target:{ value : 'ronaldo@gmail.com' } });
-        fireEvent.change(senha, {target:{ value : '123456' } });
-        
-        await userEvent.click(buttaoLogin);
+    fireEvent.change(email, { target: { value: 'ronaldo@gmail.com' } });
+    fireEvent.change(senha, { target: { value: '123456' } });
 
-        const buttaoDespesa = screen.getByRole('button', {  name: /adicionar despesa/i});
+    await userEvent.click(buttaoLogin);
 
-        expect(buttaoDespesa).toBeInTheDocument();
-    });
-})
+    const buttaoDespesa = screen.getByRole('button', { name: /adicionar despesa/i });
+
+    expect(buttaoDespesa).toBeInTheDocument();
+  });
+});
